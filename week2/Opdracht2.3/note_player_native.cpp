3d6b571dee600431cb64d4d92c74efdcf7eadd97
DylanGriffioenHU <dylan.d.griffioen@student.hu.nl>
2019-10-27 10:36:28 -0700

    cleaning

--- week2/Opdracht2.3/note_player_native.cpp
+++
@@ -1,9 +0,0 @@
-#include "hwlib.hpp"
-#include "note_player_native.hpp"
-
-  
-void note_player_native::play( const note & n ){
-   if( n.frequency != 0 ){
-        hwlib::cout << "Frequency: " << n.frequency << " Duration: " << n.duration << hwlib::endl;
-   }    
-}
